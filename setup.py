3ed9ee1db6cd172fc886167330e5028f8c87bb20
markkho <markkho32@gmail.com>
2018-05-20 15:55:13 -0700

    namechange

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='easymdp3',
+setup(name='pyrlap',
       version='0.1',
       description='Library for combining MDPs and solving them for cognitive '+
                   'science research',
-      url='https://github.com/markkho/easymdp3',
+      url='https://github.com/markkho/pyrlap',
       author="Mark Ho",
       author_email='mark.ho.cs@gmail.com',
       license='MIT',
-      packages=['easymdp3'],
+      packages=['pyrlap'],
       install_requires=[
           'markdown',
       ],
