1d9a09f743f58b9e14bb1bf1f7ba4d5027c160be
markkho <markkho32@gmail.com>
2018-06-02 19:13:06 -0400

    fixed demo notebooks and added task hierarchies to taxicab init

--- pyrlap/domains/taxicab/__init__.py
+++ pyrlap/domains/taxicab/__init__.py
@@ -1,2 +1,5 @@
 from .taxicab import *
-from pyrlap.domains.taxicab.taskhierarchies.getput import getput_hierarchy
+from .taskhierarchies.getput import getput_hierarchy
+from .taskhierarchies.rest_getput import restricted_getput
+from .taskhierarchies.simple_getput import simple_getput
+from .taskhierarchies.simple_insideoutside import simple_insideoutside
