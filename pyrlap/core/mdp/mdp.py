cba5673c8f7e4b363535882d880975426f74a0fb
markkho <markkho32@gmail.com>
2018-06-28 17:24:49 -0700

    fixed marginalization in reward sampling method, added an enumerate trajectory method

--- pyrlap/core/mdp/mdp.py
+++ pyrlap/core/mdp/mdp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
+import copy
 
-from .util import sample_prob_dict, SANSRTuple
+from pyrlap.core.util import sample_prob_dict, SANSRTuple, SANSTuple
 
 class MDP(object):
     #=============================================#
@@ -29,6 +30,15 @@ class MDP(object):
         return sample_prob_dict(self.transition_reward_dist(s, a))[0]
 
     def reward(self, s=None, a=None, ns=None):
+        if ns is not None:
+            rdist = {}
+            norm = 0
+            for (ns_, r), p in self.transition_reward_dist(s, a).items():
+                if ns_ == ns:
+                    rdist[r] = rdist.get(r, 0) + p
+                    norm += p
+            rdist = {r: p/norm for r, p in rdist.items()}
+            return sample_prob_dict(rdist)
         return sample_prob_dict(self.transition_reward_dist(s, a))[1]
 
     def reward_dist(self, s=None, a=None, ns=None):
@@ -130,3 +140,29 @@ class MDP(object):
             traj.append(SANSRTuple(s, a, ns, r))
             s = ns
         return traj
+
+    def enumerate_trajs(self, init_states=None, depth=4,
+                        no_deterministic_loops=True):
+        if init_states is None:
+            init_states = [self.get_init_state(), ]
+        trajs = set([])
+        frontier = \
+            set([tuple([SANSTuple(None, None, s),])for s in init_states])
+        while len(frontier) > 0:
+            pre_traj = list(frontier.pop())
+            s = pre_traj[-1].ns
+            if pre_traj[-1].s is None:
+                pre_traj = []
+            for a in self.available_actions(s):
+                for ns, tp in self.transition_dist(s, a).items():
+                    if no_deterministic_loops and ns == s and tp == 1:
+                        continue
+                    new_pre_traj = copy.copy(pre_traj)
+                    new_pre_traj.append(SANSTuple(s, a, ns))
+                    if self.is_terminal(ns):
+                        trajs.add(tuple(new_pre_traj))
+                    elif len(new_pre_traj) == depth:
+                        trajs.add(tuple(new_pre_traj))
+                    else:
+                        frontier.add(tuple(new_pre_traj))
+        return list(trajs)
