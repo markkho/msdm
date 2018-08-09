441a418e559c1936b3d9eafa61a147b56fceef3a
markkho <markkho32@gmail.com>
2018-08-08 20:47:48 -0700

    added bidict class

--- pyrlap/core/util.py
+++ pyrlap/core/util.py
@@ -142,3 +142,22 @@ def max_index(mylist, return_one=True):
             return np.random.choice(max_i)
     else:
         return max_i
+
+class bidict(dict):
+    def __init__(self, *args, **kwargs):
+        super(bidict, self).__init__(*args, **kwargs)
+        self.inverse = {}
+        for key, value in self.items():
+            self.inverse.setdefault(value,[]).append(key)
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.inverse[self[key]].remove(key)
+        super(bidict, self).__setitem__(key, value)
+        self.inverse.setdefault(value,[]).append(key)
+
+    def __delitem__(self, key):
+        self.inverse.setdefault(self[key],[]).remove(key)
+        if self[key] in self.inverse and not self.inverse[self[key]]:
+            del self.inverse[self[key]]
+        super(bidict, self).__delitem__(key)
