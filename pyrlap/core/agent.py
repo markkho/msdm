46d7298ef954a423362649ea4b80b18f7f50f319
markkho <markkho32@gmail.com>
2019-02-27 09:50:05 -0500

    added value function evaluator method to agent class

--- pyrlap/core/agent.py
+++ pyrlap/core/agent.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
-import logging
+import logging, warnings
+from random import randint
+from collections import Mapping
 
 from pyrlap.core.util import sample_prob_dict, calc_esoftmax_dist, SANSRTuple
 from pyrlap.core.mdp.mdp import MDP as MDPClass
 
 logger = logging.getLogger(__name__)
 
+class ValueFunction(Mapping):
+    def __init__(self, vf_dict):
+        self.vf_dict = vf_dict
+
+    def __getitem__(self, key):
+        return self.vf_dict[self.__keytransform__(key)]
+
+    def __delitem__(self, key):
+        del self.vf_dict[self.__keytransform__(key)]
+
+    def __iter__(self):
+        return iter(self.vf_dict)
+
+    def __len__(self):
+        return len(self.vf_dict)
+
+    def __keytransform__(self, key):
+        return key
 
 class Agent(object):
     def __init__(self, mdp: MDPClass):
@@ -18,8 +38,11 @@ class Agent(object):
     def act_dist(self, s, softmax_temp=None, randchoose=None):
         raise NotImplementedError
 
-    def to_dict(self) -> dict:
-        raise NotImplementedError
+    def to_dict(self, **kwargs) -> dict:
+        policy = {}
+        for s in self.mdp.get_states():
+            policy[s] = self.act_dist(s, **kwargs)
+        return policy
 
     def act(self, s, softmax_temp=None, randchoose=None):
         adist = self.act_dist(s, softmax_temp, randchoose)
@@ -48,6 +71,46 @@ class Agent(object):
                 break
             i += 1
         return traj
+
+    def value(self,
+              discount_rate=.99,
+              max_iterations=150,
+              converge_delta=.01) -> ValueFunction:
+        """
+        return the value function of this policy - basically just do
+        policy evaluation
+        """
+        vf = {}
+
+        for i in range(max_iterations):
+            change = 0
+            vf_temp = {}
+            for s in self.mdp.get_states():
+                s_val = 0
+                adist = self.act_dist(s)
+                for a, aprob in adist.items():
+                    ns_dist = self.mdp.transition_dist(s, a)
+                    for ns, nsprob in ns_dist.items():
+                        r = self.mdp.reward(s, a, ns)
+                        v = vf.get(ns, 0)
+                        s_val += (r + discount_rate*v)*aprob*nsprob
+                vf_temp[s] = s_val
+                change = max(change, abs(vf_temp[s] - vf.get(s, 0)))
+            vf = vf_temp
+            logger.debug('iteration: %d   change: %.2f' % (i, change))
+            if change < converge_delta:
+                break
+
+        if change >= converge_delta:
+            warnings.warn(
+                "Policy evaluation did not converge after %d iterations (delta=%.2f)" \
+                % (i, change))
+        return ValueFunction(vf)
+
+class RandomAgent(Agent):
+    def act_dist(self, s, softmax_temp=None, randchoose=None):
+        aa = self.mdp.available_actions(s)
+        return dict(zip(aa, [1/len(aa) for _ in range(len(aa))]))
 
 class Planner(Agent):
     def solve(self):
