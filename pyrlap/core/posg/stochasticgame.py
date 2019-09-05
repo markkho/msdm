5b9ee5c181346a7a08f5d282c6278852885025d1
markkho <markkho32@gmail.com>
2019-09-05 15:16:52 -0400

    partially observable stochastic game

---
+++ pyrlap/core/posg/stochasticgame.py
@@ -0,0 +1,25 @@
+from typing import Union, Iterable, Mapping
+
+from pyrlap.core.mdp import MDP
+from pyrlap.core.base import State, Action, Observation, Probability
+
+
+class StochasticGame(MDP):
+    """
+    A stochastic game is a modified MDP where actions
+    and rewards are tuples of length <number of agents>.
+    """
+    def __init__(self, n_agents: int):
+        self.n_agents = n_agents
+
+    def reward(self,
+               s=None,
+               a : Union([Iterable, None]) = None,
+               ns=None) -> Iterable:
+        raise NotImplementedError
+
+    def transition(self, s, a : Iterable) -> State:
+        raise NotImplementedError
+
+    def transition_dist(self, s, a: Iterable) -> Mapping[State, Probability]:
+        raise NotImplementedError
