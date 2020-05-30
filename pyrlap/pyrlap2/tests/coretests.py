17541485306670772f351194ee9eade363b5c366
markkho <markkho32@gmail.com>
2020-05-30 14:15:21 -0400

    added new test

--- pyrlap/pyrlap2/tests/coretests.py
+++ pyrlap/pyrlap2/tests/coretests.py
@@ -1,15 +1,19 @@
 import unittest
 import numpy as np
+from pyrlap.pyrlap2.core import TERMINALSTATE
 from pyrlap.pyrlap2.domains import GridWorld
+from pyrlap.pyrlap2.algorithms import VectorizedValueIteration
+
+np.seterr(divide='ignore')
 
 class CoreTestCase(unittest.TestCase):
-    def test_tabularMDP_composition(self):
+    def test_tabularMDP_ANDcomposition(self):
         gw1 = GridWorld(
             tileArray=[
                 '...g',
                 '....',
                 '.###',
-                '...s'
+                's..s'
             ],
             stepCost=-1,
             successProb=.99,
@@ -19,7 +23,7 @@ class CoreTestCase(unittest.TestCase):
         gw2 = GridWorld(
             tileArray=[
                 '##..',
-                '...g',
+                's..g',
                 '....',
                 's...'
             ],
@@ -36,6 +40,13 @@ class CoreTestCase(unittest.TestCase):
 
         #test that mdp distributions are consistent
         s0 = gw1.initialstatevec * gw2.initialstatevec
+        s0 = s0 / s0.sum()
+        eqS0 = np.isclose(gw3.initialstatevec, s0).all()
+        self.assertTrue(eqS0)
+
+        #test that less than or equal are reachable
+        rv = sum(gw1.reachablestatevec * gw2.reachablestatevec)
+        self.assertTrue(sum(gw3.reachablestatevec) <= rv)
 
         #test reward composition
         eqRF = (gw3.rewardmatrix == (gw1.rewardmatrix + gw2.rewardmatrix)).all()
@@ -48,6 +59,22 @@ class CoreTestCase(unittest.TestCase):
         self.assertTrue(eqTF)
 
     def test_runningAgentOnMDP(self):
+        gw1 = GridWorld(
+            tileArray=[
+                '...g',
+                '....',
+                '.###',
+                's..s'
+            ],
+            stepCost=-1,
+        )
+        vi = VectorizedValueIteration(temperature=.1,
+                                      entropyRegularization=True)
+        vi.planOn(gw1)
+        stateTraj = vi.runOn(gw1)['stateTraj']
+        self.assertTrue(stateTraj[-2] in gw1.absorbingStates)
+        self.assertTrue(stateTraj[-1] == TERMINALSTATE)
+        self.assertTrue(stateTraj[0] in gw1.initStates)
 
 if __name__ == '__main__':
     unittest.main()
