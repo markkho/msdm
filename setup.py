8e4f8c04b486a7ffd76066c63977682553bbb82a
markkho <markkho32@gmail.com>
2020-08-07 17:28:42 -0400

    fixing version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pyrlap',
-      version='0.3',
+      version='0.4',
       description='Library for combining MDPs and solving them for cognitive '+
                   'science research',
       url='https://github.com/markkho/pyrlap',
