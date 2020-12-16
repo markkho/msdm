583e0f8f56f532c08cbb68ef962e5a7eff9c031d
markkho <markkho32@gmail.com>
2020-12-16 12:40:21 -0500

    making grid game more accessible

--- msdm/domains/__init__.py
+++ msdm/domains/__init__.py
@@ -1,3 +1,4 @@
 from msdm.domains.gridworld.mdp import GridWorld
+from msdm.domains.gridgame.gridgame import GridGame
 from msdm.domains.modified.stickyaction import StickyActionMDP
 from msdm.domains.gridworld.stickyactiongw import StickyActionGridWorld
