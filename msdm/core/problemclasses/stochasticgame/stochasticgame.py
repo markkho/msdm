d0ec7f9cd68221a9fed3e2108db53840aafaf1b4
markkho <markkho32@gmail.com>
2021-03-13 19:27:38 -0500

    Clean up

--- msdm/core/problemclasses/stochasticgame/stochasticgame.py
+++ msdm/core/problemclasses/stochasticgame/stochasticgame.py
@@ -5,19 +5,8 @@ from msdm.core.problemclasses.problemclass import ProblemClass
 from msdm.core.distributions import Distribution
 
 class StochasticGame(ProblemClass):
-    """
-    SGs are defined by:
-    - action distributions, which bias actions at each state
-    - initial state distributions
-    - next state distributions
-    """
-
     def __init__(self, agent_names):
-        self._agentNames = agent_names
-
-    @property
-    def agent_names(self):
-        return self._agentNames
+        self.agent_names = agent_names
 
     @abstractmethod
     def initial_state_dist(self) -> Distribution:
@@ -38,47 +27,7 @@ class StochasticGame(ProblemClass):
         pass
 
     @abstractmethod
-    def joint_rewards(self,
-                      s,  # state
-                      ja,  # jointaction
-                      ns,  # nextstate
-                      ) -> Mapping[Hashable, float]:
+    def joint_rewards(self, s, ja,  ns) -> Mapping[Hashable, float]:
         """This should return a mapping from agent names to rewards"""
         pass
 
-    def __and__(self, other: "StochasticGame"):
-        """
-        Specific implementations may want to overwrite this.
-        """
-        return ANDStochasticGame(self, other)
-
-
-class ANDStochasticGame(StochasticGame):
-    """Simplest AND SG - only assumes function calls can be combined"""
-
-    def __init__(self, sg1, sg2):
-        raise NotImplementedError
-    #     self.mdp1 = mdp1
-    #     self.mdp2 = mdp2
-    #     variables = sorted(set(mdp1.variables + mdp2.variables))
-    #     MarkovDecisionProcess.__init__(self, variables)
-
-    # def getNextStateDist(self, state, action) -> Distribution:
-    #     d1 = self.mdp1.getNextStateDist(state, action)
-    #     d2 = self.mdp2.getNextStateDist(state, action)
-    #     return d1 & d2
-
-    # def getReward(self, state, action, nextstate) -> float:
-    #     r1 = self.mdp1.getReward(state, action, nextstate)
-    #     r2 = self.mdp2.getReward(state, action, nextstate)
-    #     return r1 + r2
-
-    # def getActionDist(self, state) -> Distribution:
-    #     a1 = self.mdp1.getActionDist(state)
-    #     a2 = self.mdp2.getActionDist(state)
-    #     return a1 & a2
-
-    # def getInitialStateDist(self) -> Distribution:
-    #     s1 = self.mdp1.getInitialStateDist()
-    #     s2 = self.mdp2.getInitialStateDist()
-    #     return s1 & s2
