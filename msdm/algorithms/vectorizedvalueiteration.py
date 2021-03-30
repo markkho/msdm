0af99b3cd7022c1d182b67887be754d2d1c11241
markkho <markkho32@gmail.com>
2021-03-30 07:16:10 -0400

    Add functionality to VectorizedValueIteration

--- msdm/algorithms/vectorizedvalueiteration.py
+++ msdm/algorithms/vectorizedvalueiteration.py
@@ -1,4 +1,5 @@
 from scipy.special import softmax, logsumexp
+import warnings
 import numpy as np
 from msdm.core.problemclasses.mdp import TabularPolicy, \
     TabularMarkovDecisionProcess, DeterministicTabularPolicy
@@ -6,22 +7,28 @@ from msdm.core.algorithmclasses import Plans, Result
 
 class VectorizedValueIteration(Plans):
     def __init__(self,
-                 iterations=50,
+                 iterations=None,
                  discount_rate=1.0,
                  entropy_regularization=False,
+                 convergence_diff=1e-5,
                  temperature=1.0):
         self.iters = iterations
         self.dr = discount_rate
         self.entreg = entropy_regularization
         self.temp = temperature
         self._policy = None
+        self.convergence_diff = convergence_diff
 
     def plan_on(self, mdp: TabularMarkovDecisionProcess):
+
         ss = mdp.state_list
         tf = mdp.transition_matrix
         rf = mdp.reward_matrix
         nt = mdp.nonterminal_state_vec
+        rs = mdp.reachable_state_vec
         am = mdp.action_matrix
+
+        iterations = self.iters if self.iters is not None else max(len(ss), int(1e5))
 
         #available actions - add -inf if it is not available
         aa = am.copy()
@@ -31,15 +38,22 @@ class VectorizedValueIteration(Plans):
         terminal_sidx = np.where(1 - nt)[0]
 
         v = np.zeros(len(ss))
-        for i in range(self.iters):
-            q = np.einsum("san,san->sa", tf, rf + self.dr * v)
+        for i in range(iterations):
+            q = np.einsum("san,san->sa", tf, rf + self.dr * v[None, None, :])
             if self.entreg:
-                v = self.temp * logsumexp((1 / self.temp) * q + np.log(am),
+                nv = self.temp * logsumexp((1 / self.temp) * q + np.log(am),
                                           axis=-1)
-                v[terminal_sidx] = 0 #terminal states are always 0 reward
+                nv[terminal_sidx] = 0 #terminal states are always 0 reward
             else:
-                v = np.max(q + aa, axis=-1)
-                v[terminal_sidx] = 0 #terminal states are always 0 reward
+                nv = np.max(q + aa, axis=-1)
+                nv[terminal_sidx] = 0 #terminal states are always 0 reward
+
+            diff = (v - nv)*rs
+            if np.abs(diff).max() < self.convergence_diff:
+                break
+            v = nv
+        if i == (iterations - 1):
+            warnings.warn(f"VI not converged after {iterations} iterations")
         if self.entreg:
             pi = softmax((1 / self.temp) * q, axis=-1)
         else:
@@ -53,7 +67,7 @@ class VectorizedValueIteration(Plans):
         # create result object
         res = Result()
         res.mdp = mdp
-        cls = TabularPolicy if self.entreg else DeterministicTabularPolicy
+        cls = TabularPolicy if ((pi < 1) & (pi > 0)).any() else DeterministicTabularPolicy
         res.policy = res.pi = cls(mdp.state_list, mdp.action_list, policy_matrix=pi, mdp=mdp)
         res._valuevec = v
         vf = mdp.state_map()
@@ -62,6 +76,7 @@ class VectorizedValueIteration(Plans):
         res.valuefunc = res.V = vf
         res._qvaluemat = q
         res.iterations = i
+        res.max_bellman_error = diff
         qf = mdp.state_action_map()
         for si, s in enumerate(mdp.state_list):
             for ai, a in enumerate(mdp.action_list):
