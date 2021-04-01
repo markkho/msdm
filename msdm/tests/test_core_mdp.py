a5490700c9e00054c6cd443c6be718839b18e8ee
Carlos Correa <cgcorrea@princeton.edu>
2021-04-01 16:51:40 -0400

    Extend QuickTabularMDP

--- msdm/tests/test_core_mdp.py
+++ msdm/tests/test_core_mdp.py
@@ -48,17 +48,27 @@ class CoreTestCase(unittest.TestCase):
         from msdm.core.distributions import DictDistribution as DD
         from msdm.core.problemclasses.mdp.quicktabularmdp import QuickTabularMDP
         TERMINAL = -float('inf')
-        mdp = QuickTabularMDP(
-            next_state_dist    = lambda s, a : DD({s + a: .8, s - a: .1, s: .1 if a else 1}) if (0 < s < 10) else DD({TERMINAL: 1}),
-            reward             = lambda s, a, ns : {0: -100, 10: 0, TERMINAL: 0}.get(ns, 0) - abs(a) - 1,
-            actions            = lambda s: (-1, 0, 1),
-            initial_state_dist = lambda : DD({5: .5, 6: .5}),
-            is_terminal        = lambda s: s == TERMINAL,
-            discount_rate=.99
-        )
-        res = VectorizedValueIteration().plan_on(mdp)
-        pi = [res.policy.action_dist(s).sample() for s in range(1, 10)]
-        assert all([a == 1 for a in pi])
+        for mdp in [
+            QuickTabularMDP(
+                next_state_dist    = lambda s, a : DD({s + a: .8, s - a: .1, s: .1 if a else 1}) if (0 < s < 10) else DD({TERMINAL: 1}),
+                reward             = lambda s, a, ns : {0: -100, 10: 0, TERMINAL: 0}.get(ns, 0) - abs(a) - 1,
+                actions            = lambda s: (-1, 0, 1),
+                initial_state_dist = lambda : DD({5: .5, 6: .5}),
+                is_terminal        = lambda s: s == TERMINAL,
+                discount_rate      = .99
+            ),
+            QuickTabularMDP(
+                next_state         = lambda s, a : (s + a) if (0 < s < 10) else s,
+                reward             = -1,
+                actions            = (-1, 0, 1),
+                initial_state      = 1,
+                is_terminal        = lambda s: s == 10,
+                discount_rate      = .99
+            ),
+        ]:
+            res = VectorizedValueIteration().plan_on(mdp)
+            pi = [res.policy.action_dist(s).sample() for s in range(1, 10)]
+            assert all([a == 1 for a in pi])
 
 if __name__ == '__main__':
     unittest.main()
