ae840b8051e4e83804d355f28aba56b5a6b8efbd
markkho <markkho32@gmail.com>
2021-10-18 23:54:34 -0400

    Tiger problem

---
+++ msdm/domains/tiger.py
@@ -0,0 +1,38 @@
+from msdm.core.problemclasses.pomdp import TabularPOMDP
+from msdm.core.distributions import DictDistribution
+class Tiger(TabularPOMDP):
+    discount_rate : float = 0.95
+
+    def __init__(self, coherence):
+        self.coherence = coherence
+
+    def next_state_dist(self, s, a):
+        if a == 'listen':
+            return DictDistribution.deterministic(s)
+        return self.initial_state_dist()
+
+    def reward(self, s, a, ns):
+        if a == 'listen':
+            return -1
+        elif s == a:
+            return -100
+        else:
+            return 10
+
+    def actions(self, s):
+        return ['left', 'right', 'listen']
+
+    def initial_state_dist(self):
+        return DictDistribution.uniform(['left', 'right'])
+
+    def is_terminal(self, s):
+        return False
+
+    def observation_dist(self, a, ns):
+        if a != 'listen':
+            pleft = 0.5
+        elif ns == 'left':
+            pleft = self.coherence
+        else:
+            pleft = 1 - self.coherence
+        return DictDistribution(left=pleft, right=1-pleft)
