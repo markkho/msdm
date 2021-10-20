bba08546592e5fbe104df87bd4ceff06e86220e3
markkho <markkho32@gmail.com>
2021-10-20 18:52:24 -0400

    Reorganize

--- msdm/algorithms/__init__.py
+++ msdm/algorithms/__init__.py
@@ -4,4 +4,4 @@ from msdm.algorithms.lrtdp import LRTDP
 from msdm.algorithms.policyiteration import PolicyIteration
 from msdm.algorithms.search import BreadthFirstSearch, AStarSearch
 from msdm.algorithms.entregpolicyiteration import EntropyRegularizedPolicyIteration
-from msdm.algorithms.qlearning import QLearning
+from msdm.algorithms.tdlearning import QLearning, SARSA, ExpectedSARSA
