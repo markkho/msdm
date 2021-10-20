52961e6c300807dfd58d2540c2e99d887ea6f6ce
markkho <markkho32@gmail.com>
2021-10-20 16:49:48 -0400

    Refactor Q-learning into TD-learning

--- msdm/algorithms/qlearning.py
+++ msdm/algorithms/qlearning.py
@@ -1,10 +1,27 @@
 from msdm.core.algorithmclasses import Learns, Result
 from msdm.core.problemclasses.mdp import TabularMarkovDecisionProcess, TabularPolicy
+from msdm.core.distributions import DictDistribution
 from collections import defaultdict
+from abc import abstractmethod
 import random
 import math
 
-class QLearning(Learns):
+def epsilon_softmax(action_values, rand_choose, softmax_temp, rng):
+    aa, qs = zip(*action_values.items())
+    if (rand_choose) and (rng.random() < rand_choose):
+        a = rng.choice(aa)
+    else:
+        if softmax_temp != 0.0:
+            weights = [math.exp(qi/softmax_temp) for qi in qs]
+            tot = sum(weights)
+            weights = [w/tot for w in weights]
+            a = rng.choices(aa, weights=weights, k=1)[0]
+        else:
+            maxq = max(qs)
+            a = rng.choice([a for a in aa if action_values[a] == maxq])
+    return a
+
+class TemporalDifferenceLearning(Learns):
     def __init__(
         self,
         episodes=100,
@@ -30,13 +47,47 @@ class QLearning(Learns):
         self.softmax_temp = softmax_temp
         self.seed = seed
 
-    def train_on(self, mdp: TabularMarkovDecisionProcess):
-        # initialize random number generator
+    @abstractmethod
+    def _training(self, mdp, rng):
+        pass
+
+    def _init_random_number_generator(self):
         if self.seed is not None:
             rng = random.Random(self.seed)
         else:
             rng = random
+        return rng
 
+    def _create_policy(self, mdp, q):
+        policy = {}
+        for s in mdp.state_list:
+            if s not in q:
+                max_aa = mdp.actions(s)
+            else:
+                maxq = max(q[s].values())
+                max_aa = [a for a in q[s].keys() if q[s][a] == maxq]
+            policy[s] = DictDistribution({a: 1/len(max_aa) for a in max_aa})
+        policy = TabularPolicy(policy)
+        return policy
+
+    def train_on(self, mdp: TabularMarkovDecisionProcess):
+        rng = self._init_random_number_generator()
+        q, episode_rewards = self._training(mdp, rng)
+        return Result(
+            episode_rewards=episode_rewards,
+            q_values=q,
+            policy=self._create_policy(mdp, q, rng)
+        )
+
+class QLearning(TemporalDifferenceLearning):
+    r"""
+    Q-learning is an off-policy temporal difference control method.
+    The temporal difference error in q-learning is:
+    $$
+    \delta_t = R_{t+1} + \gamma\max_a Q(S_{t+1}, a) - Q(S_t, A_t)
+    $$
+    """
+    def _training(self, mdp, rng):
         q = {}
         initial_q = 0
         episode_rewards = []
@@ -46,21 +97,10 @@ class QLearning(Learns):
             while not mdp.is_terminal(s):
                 # initialize q values if state hasn't been visited
                 if s not in q:
-                    aa = mdp.actions(s)
-                    q[s] = {a: initial_q for a in aa}
-                # choose according to an epsilon-softmax policy
-                if (self.rand_choose) and (rng.random() < self.rand_choose):
-                    a = rng.choice(aa)
-                else:
-                    if self.softmax_temp != 0.0:
-                        aa, qs = zip(*q[s].items())
-                        weights = [math.exp(qi/self.softmax_temp) for qi in qs]
-                        tot = sum(weights)
-                        weights = [w/tot for w in weights]
-                        a = rng.choices(aa, weights=weights, k=1)[0]
-                    else:
-                        maxq = max(q[s].values())
-                        a = rng.choice([a for a in q[s].keys() if q[s][a] == maxq])
+                    q[s] = {a: initial_q for a in mdp.actions(s)}
+
+                # select action
+                a = epsilon_softmax(q[s], self.rand_choose, self.softmax_temp, rng)
 
                 # transition to next state
                 ns = mdp.next_state_dist(s, a).sample(rng=rng)
@@ -72,18 +112,4 @@ class QLearning(Learns):
                 ep_reward += r
                 s = ns
             episode_rewards.append(ep_reward)
-
-        policy = {}
-        for s in mdp.state_list:
-            if s not in q:
-                policy[s] = rng.choice(mdp.actions(s))
-                continue
-            maxq = max(q[s].values())
-            policy[s] = rng.choice([a for a in q[s].keys() if q[s][a] == maxq])
-        policy = TabularPolicy.from_deterministic_map(policy)
-
-        return Result(
-            episode_rewards=episode_rewards,
-            q_values=q,
-            policy=policy
-        )
+        return q, episode_rewards
