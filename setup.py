dc4db4eed04ba584dd5aa8e870760d8f4246c3a0
markkho <markkho32@gmail.com>
2021-10-21 12:37:17 -0400

    Fix dependencies

--- setup.py
+++ setup.py
@@ -4,18 +4,26 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='msdm',
-      version='0.4',
-      description='Models of sequential decision making',
-      url='https://github.com/markkho/msdm',
-      author="Mark Ho",
-      author_email='mark.ho.cs@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-          'numpy', 'matplotlib', 'scipy', 'torch', 'tqdm', 'sparse', 'termcolor',
-          'frozendict @ git+https://github.com/Marco-Sulla/python-frozendict@v2.0.1'
-      ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      zip_safe=False)
+setup(
+    name='msdm',
+    version='0.4',
+    description='Models of sequential decision making',
+    url='https://github.com/markkho/msdm',
+    author="Mark Ho",
+    author_email='mark.ho.cs@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'torch',
+        'tqdm',
+        'pandas',
+        'frozendict',
+        'termcolor',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=False
+)
