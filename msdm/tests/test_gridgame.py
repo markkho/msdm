812534de6a8357792a9da26983357d3cd2ba3b02
markkho <markkho32@gmail.com>
2021-10-21 12:25:27 -0400

    Clean up multi-agent support
    The current implementations of multi-agent learning are not
    fully tested or entirely consistent with the rest of the
    package. It also relies on the `sparse` library, which
    has conflicts with the most updated version of `numpy`
    via `numba`. For simplicity, I am removing references to
    multi-agent parts of the library that cause problems
    and including a warning when `TabularStochasticGame`
    is called.

--- msdm/tests/test_gridgame.py
+++ msdm/tests/test_gridgame.py
@@ -2,7 +2,6 @@ import unittest
 import numpy as np
 from msdm.domains import GridWorld
 from msdm.domains.gridgame.tabulargridgame import TabularGridGame
-from msdm.algorithms import ValueIteration
 
 np.seterr(divide='ignore')
 
@@ -10,7 +9,7 @@ class TestCase(unittest.TestCase):
     def test_gridgame_initialization(self):
         #example usage
         gameString = """
-            #  # # #  G0 #  # # # 
+            #  # # #  G0 #  # # #
             G0 . . A0 .  A1 . . G1
             #  # # #  G1 #  # # #
         """.strip()
@@ -22,16 +21,16 @@ class TestCase(unittest.TestCase):
         ns = nsdist.sample()
         r = gg.joint_rewards(s, a, ns)
         self.assertTrue(True)
-    
+
     def test_TabularGridGame(self):
         gamestring = """
         # # # # # # # #
         # A0 . . . . A1 #
-        # . . . . . . # 
+        # . . . . . . #
         # u u . . u u #
-        # . . . . . . # 
+        # . . . . . . #
         # G1 . . . G0 . #
-        # # # # # # # # 
+        # # # # # # # #
         """.strip()
         gg = TabularGridGame(gamestring)
         init_state = gg.initial_state_dist().sample()
@@ -39,7 +38,6 @@ class TestCase(unittest.TestCase):
         joint_action = {agent: next(actions) for agent, actions in joint_actions.items()}
         next_state = gg.next_state_dist(init_state,joint_action).sample()
         rewards = gg.joint_rewards(init_state,joint_action,next_state)
-        
+
 if __name__ == '__main__':
     unittest.main()
-
