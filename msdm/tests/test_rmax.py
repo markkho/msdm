2f7d06b74a58bcbc5b97faa989e9989957857b27
markkho <markkho32@gmail.com>
2022-09-20 11:33:46 -0400

    Failing RMAX test

--- msdm/tests/test_rmax.py
+++ msdm/tests/test_rmax.py
@@ -1,4 +1,5 @@
-from msdm.algorithms import RMAX
+import numpy as np
+from msdm.algorithms import RMAX, PolicyIteration
 from msdm.tests.domains import make_russell_norvig_grid
 
 
@@ -12,3 +13,9 @@ def test_rmax():
         seed=0,
     )
     res = rmax.train_on(gw)
+    pi_res = PolicyIteration().plan_on(gw)
+
+    for s, a_qval in res.q_values.items():
+        for a, qval in a_qval.items():
+            exp_qval = pi_res.actionvaluefunc[s][a]
+            assert np.isclose(exp_qval, qval), f"{s}, {a}, actual: {qval}, expected: {exp_qval}"
