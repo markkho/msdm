f5853a6a2bbed485a6805598e5e84af8dcb3711d
markkho <markkho32@gmail.com>
2022-10-16 14:48:39 -0400

    Refactor and clean up Table class and tests

--- msdm/core/table.py
+++ msdm/core/table.py
@@ -1,110 +1,105 @@
+from abc import ABC, abstractmethod, abstractproperty
+from collections import namedtuple
+from multiprocessing.sharedctypes import Value
 import numpy as np
 from itertools import product
-from typing import Hashable, Mapping, Sequence, Union, Tuple
+from typing import Hashable, Mapping, Sequence, Union, Tuple, TypeVar, Any
 from msdm.core.distributions.dictdistribution import DictDistribution
 
-class Table(np.lib.mixins.NDArrayOperatorsMixin):
-    """
-    A table maps an unordered set of multi-dimensional 
-    keys to real values. It can be thought of as a
-    np.array with labeled dimensions and indices.
-    """
-    def __init__(
-        self,
-        data : np.array,
-        dims : Sequence[str],
-        coords : Union[Tuple[Sequence[Hashable]], Mapping[str, Sequence[Hashable]]],
-    ):
-        self._data = data
-        self._data.setflags(write=False)
-        self._dims = dims
-        self._coords = coords
-        self._column_dims_idx = -1
-    
-    def _validate_table(self):
-        assert isinstance(self._coords, (tuple, list))
-        assert isinstance(self._dims, (tuple, list))
-        assert len(self._coords) == len(self._dims), "Number of coordinates and dimensions don't match"
-        coords_shape = tuple([len(c) for c in self._coords])
-        unique_shape = tuple([len(set(c)) for c in self._coords])
-        data_shape = self._data.shape
-        if not (data_shape == coords_shape == unique_shape):
-            raise ValueError(
-                f"Total coordinates ({coords_shape}) or unique coordinats ({unique_shape})" + \
-                f" do not match dimension lengths ({data_shape})."
-            )
-        
-    # dict-like interface
-    def __getitem__(self, key):
-        if not isinstance(key, (tuple, list)):
-            keys = (key,)
-        elif key in self._coords[0]:
-            keys = (key,)
-        else:
-            keys = key
-        idx = self._key_indices(keys)
-        if len(idx) == len(self._data.shape): #array element
-            return self._data[idx]
-        return self._get_subtable(idx)
-    def _key_indices(self, keys):
-        idx = tuple([coords.index(k) for coords, k in zip(self._coords, keys)])
-        return idx
-    def _get_subtable(self, coords_idx):
-        return Table(
-            data=self._data[coords_idx],
-            dims=self._dims[len(coords_idx):],
-            coords=self._coords[len(coords_idx):],
-        )
-    def items(self):
+from msdm.core.utils.funcutils import cached_property
+
+TableEntry = TypeVar("TableEntry")
+IndexValue = TypeVar("IndexValue")
+
+class AbstractTable(ABC):
+    # dictionary-like interface
+    @abstractmethod
+    def __getitem__(self, key: Union[IndexValue,Tuple[IndexValue]]) -> Union[TableEntry,"AbstractTable"]:
+        """
+        `key` can be a single outermost-variable value of the index,
+        or a tuple of variable values, in order of the variables.
+        The return value is either a single table entry or another table.
+        """
+        pass
+    @abstractmethod
+    def __len__(self):
+        pass
+    @abstractmethod
+    def keys(self) -> Sequence[IndexValue]:
+        """
+        Generates all outer keys in a non-guaranteed order
+        """
+        pass
+    def __iter__(self) -> Sequence[IndexValue]:
+        yield from self.keys()
+    def items(self) -> Sequence[Tuple[IndexValue,Union[TableEntry,"AbstractTable"]]]:
         yield from ((k, self[k]) for k in self.keys())
-    def keys(self):
-        yield from self._coords[0]
-    def values(self):
+    def values(self) -> Sequence[Union[TableEntry,"AbstractTable"]]:
         yield from (self[k] for k in self.keys())
-    def get(self, key, default=None):
+    def get(self, key : IndexValue, default=None) -> Any:
         try:
             return self[key]
         except KeyError:
             return default
-    def __len__(self):
-        return len(self._coords[0])
-    def __iter__(self):
-        yield from self.keys()
-    def isclose(
-        self, other: "Table", *,
-        # These tolerances are copied from `np.isclose()`
-        rtol=1e-05, atol=1e-08,
-    ) -> bool:
-        return (
-            self.shape == other.shape and \
-            self._dims == other._dims and \
-            all([c1 == c2 for c1, c2 in zip(self._coords, other._coords)]) and \
-            np.isclose(self._data, other._data, atol=atol, rtol=rtol).all()
-        )
     
-    # indirect support for np.array-like interface
+    # np.array-like interface (currently limited)
+    @abstractmethod
+    def __array__(self, dtype=None):
+        pass
+    def __array_ufunc__(self, ufunc, method, *args, **kws):
+        raise NotImplementedError
     def __getattr__(self, attr):
         if attr in ("shape", "ndim"):
-            return getattr(self._data, attr)
+            return getattr(self.__array__(), attr)
         else:
             raise AttributeError(f"'{self.__class__}' object has no attribute '{attr}'")    
-    def __array__(self, dtype=None):
-        return self._data
     
-    def __repr__(self):
-        return f"{self.__class__.__name__}(" +\
-            f"data={repr(self._data)},\n" + \
-            f"coords={repr(self._coords)},\n" + \
-            f"dims={repr(self._dims)})"
-    
+    # AbstractTable interface
+    @abstractproperty
+    def table_index(self) -> "TableIndex": pass
+
+IndexVariable = namedtuple("IndexVariable", "name values")
+class TableIndex:
+    """
+    An ordered collection of named variables representing
+    the indexing scheme of a table.
+    """
+    def __init__(self, variables : Sequence[IndexVariable]):
+        self._variables = variables
+    def __getitem__(self, variable_index):
+        return self._variables[variable_index]
+    @cached_property
+    def variable_names(self):
+        return tuple([v.name for v in self._variables])
+    @cached_property
+    def variables(self):
+        return tuple(self._variables)
+    @cached_property
+    def variable_values(self):
+        return tuple([v.values for v in self._variables])
+    def product(self):
+        yield from product(*self.variable_values)
+    def product_dicts(self):
+        for assignments in self.product():
+            yield dict(zip(self.variable_names, assignments))
+    def equivalent_to(self, other):
+        return (
+            self.variable_names == other.variable_names and \
+            all([c1 == c2 for c1, c2 in zip(self.variable_values, other.variable_values)])
+        )
+
+class Table_repr_html_MixIn(AbstractTable):
+    _column_dims_idx = -1 #this is for interpreting a table as a matrix
     def _repr_html_(self):
         import pandas as pd
         pivot = self._column_dims_idx
+        variable_names = self.table_index.variable_names
+        variable_values = self.table_index.variable_values
         df_data = []
-        row_dims = self._dims[:pivot]
-        col_dims = self._dims[pivot:]
-        row_coords = self._coords[:pivot]
-        col_coords = self._coords[pivot:]
+        row_dims = variable_names[:pivot]
+        col_dims = variable_names[pivot:]
+        row_coords = variable_values[:pivot]
+        col_coords = variable_values[pivot:]
         df_index = []
         df_cols = list(product(*col_coords))
         df_index = list(product(*row_coords))
@@ -129,21 +124,106 @@ class Table(np.lib.mixins.NDArrayOperatorsMixin):
         ).to_html()
         return html_table
 
-class ProbabilityTable(Table):
-    # Probability tables represent and return distributions 
-    # if indexed into a column dimension but otherwise behave like tables
-    def _get_subtable(self, coords_idx):
-        assert self._column_dims_idx in (-1, self.ndim - 1), \
-            f"Currently, for {self.__class__.__name__}, only the last dimension can be a column dimension " +\
-            f" but column dimensions start at {self._column_dims_idx}"
-        result_dimensionality = len(self._data.shape) - len(coords_idx)
-        if result_dimensionality == 1:
-            return TableDistribution(
-                data=self._data[coords_idx],
-                dims=self._dims[len(coords_idx):],
-                coords=self._coords[len(coords_idx):]
+class Table(Table_repr_html_MixIn,AbstractTable):
+    """
+    A table backed by a numpy array.
+    """
+    _perform_table_validations = True
+    def __init__(
+        self,
+        data : np.ndarray,
+        variable_names : Sequence[Hashable],
+        variable_values : Sequence[Tuple[Hashable]],
+    ):
+        self._data = data
+        self._data.setflags(write=False)
+        if len(variable_names) != len(variable_values):
+            raise ValueError("Different numbers of variable names and values")
+        self._index = TableIndex(
+            [IndexVariable(n, v) for n, v in zip(variable_names, variable_values)]
+        )
+        if self._perform_table_validations:
+            self._validate_table()
+
+    def _validate_table(self):
+        coords_shape = tuple([len(c) for c in self.table_index.variable_values])
+        unique_shape = tuple([len(set(c)) for c in self.table_index.variable_values])
+        data_shape = self._data.shape
+        if not (data_shape == coords_shape == unique_shape):
+            raise ValueError(
+                f"Total coordinates ({coords_shape}) or unique coordinats ({unique_shape})" + \
+                f" do not match dimension lengths ({data_shape})."
             )
-        return Table._get_subtable(self, coords_idx)
+        
+    # dict-like interface
+    def __getitem__(self, table_key):
+        if not isinstance(table_key, (tuple, list)):
+            table_keys = (table_key,)
+        elif table_key in self.table_index.variable_values[0]:
+            table_keys = (table_key,)
+        else:
+            table_keys = table_key
+        array_idx = self._array_index(table_keys)
+        if len(array_idx) == len(self._data.shape): #array element
+            return self._data[array_idx]
+        return self._get_subtable(array_idx)
+    def _array_index(self, keys):
+        try:
+            idx = [vals.index(assn) for vals, assn in zip(self.table_index.variable_values, keys)]
+        except ValueError:
+            raise ValueError(f"{keys} is not in Table")
+        return tuple(idx)
+    def _get_subtable(self, array_idx):
+        return Table(
+            data=self._data[array_idx],
+            variable_names=self.table_index.variable_names[len(array_idx):],
+            variable_values=self.table_index.variable_values[len(array_idx):],
+        )
+    def keys(self):
+        yield from self.table_index.variable_values[0]
+    def __len__(self):
+        return len(self.table_index.variable_values[0])
+    
+    # np.array-like interface
+    def __array__(self, dtype=None):
+        return self._data
+    @property
+    def table_index(self):
+        return self._index
+    
+    def equivalent_to(
+        self, other: "Table", *,
+        # These tolerances are copied from `np.isclose()`
+        rtol=1e-05, atol=1e-08,
+    ) -> bool:
+        return (
+            self.shape == other.shape and \
+            self.table_index.equivalent_to(other.table_index) and \
+            np.isclose(self._data, other._data, atol=atol, rtol=rtol).all()
+        )
+
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(" +\
+            f"data={repr(self._data)},\n" + \
+            f"variable_names={repr(self.table_index.variable_names)},\n" + \
+            f"variable_values={repr(self.table_index.variable_values)})"
+
+class ProbabilityTable(Table):
+    #the starting dimension over which entries are a single probability distribution
+    probs_start_index = -1 
+    def _get_subtable(self, array_idx):
+        if self.probs_start_index < 0:
+            probs_start_index = len(self._data.shape) + self.probs_start_index
+        else:
+            probs_start_index = self.probs_start_index
+        if len(array_idx) >= probs_start_index:
+            return TableDistribution(
+                data=self._data[array_idx],
+                variable_names=self.table_index.variable_names[len(array_idx):],
+                variable_values=self.table_index.variable_values[len(array_idx):],
+            )
+        return Table._get_subtable(self, array_idx)
 
 class TableDistribution(Table,DictDistribution):
     """DictDistribution backed by a numpy array."""
--- msdm/tests/test_core_table.py
+++ msdm/tests/test_core_table.py
@@ -1,6 +1,38 @@
 import numpy as np
-from msdm.core.table import Table, ProbabilityTable
-from msdm.core.distributions import DictDistribution
+from msdm.core.table import Table, ProbabilityTable, IndexVariable, TableIndex
+from msdm.core.distributions import Distribution 
+
+def test_TableIndex():
+    variables = [
+        IndexVariable('a', (0, 1)), 
+        IndexVariable('b', ("x", "y", "z"))
+    ]
+    idx = TableIndex(variables)
+    assert idx.variable_names == ('a', 'b')
+    for vi, v in enumerate(idx.variables):
+        assert v.name == variables[vi].name
+        assert v.values == variables[vi].values
+    assert idx.variable_values == ((0, 1), ("x", "y", "z"))
+    assert set(idx.product()) == set([
+        (0, 'x'),
+        (0, 'y'),
+        (0, 'z'),
+        (1, 'x'),
+        (1, 'y'),
+        (1, 'z'),
+    ])
+    # product_dict should return exactly these dictionaries
+    exp_dicts = [
+        dict(a=0, b='x'),
+        dict(a=0, b='y'),
+        dict(a=0, b='z'),
+        dict(a=1, b='x'),
+        dict(a=1, b='y'),
+        dict(a=1, b='z'),
+    ]
+    for d in idx.product_dicts():
+        exp_dicts.pop(exp_dicts.index(d))
+    assert len(exp_dicts) == 0
 
 def test_Table_construction_and_writing():
     # Can we construct a Table
@@ -8,10 +40,10 @@ def test_Table_construction_and_writing():
     tb_vals = np.random.random((5, 3))
     tb = Table(
         data=tb_vals.copy(),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b', 'c', 'd', 'e'],
-            ['x', 'y', 'z']
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b', 'c', 'd', 'e'),
+            ('x', 'y', 'z')
         )
     )
     
@@ -20,12 +52,12 @@ def test_Table_construction_and_writing():
     try:
         Table(
             data=np.random.random((5, 3)),
-            dims=("dim1", "dim2"),
-            coords=(
-                ['a', 'b', 'd', 'e'],
-                ['x', 'y', 'z']
+            variable_names=("dim1", "dim2"),
+            variable_values=(
+                ('a', 'b', 'd', 'e'),
+                ('x', 'y', 'z')
             )
-        )._validate_table()
+        )
         assert False
     except ValueError:
         pass
@@ -34,12 +66,12 @@ def test_Table_construction_and_writing():
     try:
         Table(
             data=np.random.random((5, 3)),
-            dims=("dim1", "dim2"),
-            coords=(
-                ['a', 'b', 'c', 'd', 'e', 'f'],
-                ['x', 'y', 'z']
+            variable_names=("dim1", "dim2"),
+            variable_values=(
+                ('a', 'b', 'c', 'd', 'e', 'f'),
+                ('x', 'y', 'z')
             )
-        )._validate_table()
+        )
         assert False
     except ValueError:
         pass
@@ -48,12 +80,12 @@ def test_Table_construction_and_writing():
     try:
         Table(
             data=np.random.random((5, 3)),
-            dims=("dim1", "dim2"),
-            coords=(
-                ['a', 'b', 'b', 'd', 'e'],
-                ['x', 'y', 'z']
+            variable_names=("dim1", "dim2"),
+            variable_values=(
+                ('a', 'b', 'b', 'd', 'e'),
+                ('x', 'y', 'z')
             )
-        )._validate_table()
+        )
         assert False
     except ValueError:
         pass
@@ -64,10 +96,10 @@ def test_Table_construction_and_writing():
             [1, 2, 3],
             [4, 5, 6],
         ]),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b'],
-            ['x', 'y', 'z']
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b'),
+            ('x', 'y', 'z')
         )
     )
     tb2 = Table(
@@ -75,10 +107,10 @@ def test_Table_construction_and_writing():
             [0, 1, 2],
             [3, 4, 5],
         ]) + 1,
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b'],
-            ['x', 'y', 'z']
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b'),
+            ('x', 'y', 'z')
         )
     )
     tb3 = Table(
@@ -86,10 +118,10 @@ def test_Table_construction_and_writing():
             [0, 1, 2],
             [3, 4, 5],
         ]),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b'],
-            ['x', 'y', 'z']
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b'),
+            ('x', 'y', 'z')
         )
     )
     tb4 = Table(
@@ -97,16 +129,16 @@ def test_Table_construction_and_writing():
             [1, 2, 3],
             [4, 5, 6],
         ]),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['j', 'k'],
-            ['x', 'y', 'z']
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('j', 'k'),
+            ('x', 'y', 'z')
         )
     )
-    assert tb1.isclose(tb2)
-    assert tb2.isclose(tb1)
-    assert not tb1.isclose(tb3)
-    assert not tb1.isclose(tb4)
+    assert tb1.equivalent_to(tb2)
+    assert tb2.equivalent_to(tb1)
+    assert not tb1.equivalent_to(tb3)
+    assert not tb1.equivalent_to(tb4)
     assert np.isclose(tb1._data, tb4._data).all()
 
     # blocking write to underlying array
@@ -117,26 +149,37 @@ def test_Table_construction_and_writing():
         pass
     # test repr
     tb1_repr = eval(repr(tb1), {**globals(), 'array': np.array})
-    assert tb1_repr.isclose(tb1)
+    assert tb1_repr.equivalent_to(tb1)
     
 def test_Table_array_like_interface():
     np.random.seed(1201)
+    data = np.random.random((5, 3))
     tb = Table(
-        data=np.random.random((5, 3)),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b', 'c', 'd', 'e'],
-            ['x', 'y', 'z']
+        data=data,
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b', 'c', 'd', 'e'),
+            ('x', 'y', 'z')
         )
     )
+    # Accessing elements in an array-like way
+    assert tb["d", "y"] == data[3, 1]
+
+    # Throw error when accessing a non-existent key
+    tb["d"]
+    try:
+        tb["d", "w"]
+        assert False
+    except ValueError:
+        pass
+
     # Support for some numpy attributes on object
     assert tb.shape == (5, 3)
     assert tb.ndim == 2
 
-    # Can we run numpy functions via __array__() and get back an np.array?
-    assert np.mean(tb) == tb._data.mean()
-    assert isinstance(np.log(tb), np.ndarray)
-    assert isinstance(tb * 2, np.ndarray)
+    # Castng to a numpy array
+    assert isinstance(np.array(tb), np.ndarray)
+
     
 def test_Table_dict_like_interface():
     np.random.seed(1201)
@@ -146,10 +189,10 @@ def test_Table_dict_like_interface():
             [4, 5, 6],
 
         ]),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b'],
-            ['x', 'y', 'z'],
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b'),
+            ('x', 'y', 'z'),
         ),
     )
 
@@ -170,11 +213,11 @@ def test_Table_dict_like_interface():
                 [0, 1, 8],
             ],
         ]),
-        dims=("dim1", "dim2", "dim3"),
-        coords=(
-            ['a', 'b'],
-            [(6,), (9,)],
-            ['x', 'y', 'z'],
+        variable_names=("dim1", "dim2", "dim3"),
+        variable_values=(
+            ('a', 'b'),
+            ((6,), (9,)),
+            ('x', 'y', 'z'),
         ),
     )
     tb_2d = Table(
@@ -182,17 +225,17 @@ def test_Table_dict_like_interface():
             [1, 2, 3],
             [4, 5, 6],
         ]),
-        dims=("dim2", "dim3"),
-        coords=(
-            [(6,), (9,)],
-            ['x', 'y', 'z'],
+        variable_names=("dim2", "dim3"),
+        variable_values=(
+            ((6,), (9,)),
+            ('x', 'y', 'z'),
         ),
     )
-    assert tb_3d['a'].isclose(tb_2d)
+    assert tb_3d['a'].equivalent_to(tb_2d)
 
     # Can we iterate over outermost dimension with items()?
     for key, value in tb.items():
-        assert tb[key].isclose(value)
+        assert tb[key].equivalent_to(value)
 
 def test_ProbabilityTable_and_TableDistribution():
     # Can we access innermost dimension as a Distribution 
@@ -202,11 +245,30 @@ def test_ProbabilityTable_and_TableDistribution():
             [.5, .25, .25],
             [1/3, 1/3, 1/3],
         ]),
-        dims=("dim1", "dim2"),
-        coords=(
-            ['a', 'b'],
-            ['x', 'y', 'z'],
+        variable_names=("dim1", "dim2"),
+        variable_values=(
+            ('a', 'b'),
+            ('x', 'y', 'z'),
         ),
     )
-    assert isinstance(tb_probs['a'], DictDistribution)
+    assert isinstance(tb_probs['a'], Distribution)
     assert isinstance(tb_probs['a'], Table)
+
+    probs = np.random.random((5, 5, 4, 3))
+    probs = probs/probs.sum(axis=(-1, -2), keepdims=True)
+    tb_4d = ProbabilityTable(
+        data=probs,
+        variable_names=("dim1", "dim2", "dim3", "dim4"),
+        variable_values=(
+            tuple("abcde"),
+            tuple("fghij"),
+            tuple("klmn"),
+            tuple("xyz"),
+        ),
+    )
+    tb_4d.probs_start_index = -2
+    assert not isinstance(tb_4d, Distribution)
+    assert not isinstance(tb_4d['c'], Distribution)
+    assert isinstance(tb_4d['c', 'g'], Distribution)
+    assert isinstance(tb_4d['c', 'g', 'n'], Distribution)
+    assert not isinstance(tb_4d['c', 'g', 'n', 'z'], Distribution)
