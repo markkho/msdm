839c8108188b1a826a6e7851532bf253f6ac4092
markkho <markkho32@gmail.com>
2022-10-31 15:45:46 -0400

    Clean up policy iteration and refactor to batched version

--- msdm/algorithms/policyiteration.py
+++ msdm/algorithms/policyiteration.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Sequence
 import warnings
 import numpy as np
 
@@ -8,7 +9,6 @@ from msdm.core.algorithmclasses import Plans, PlanningResult
 from msdm.core.mdp_tables import StateActionTable, StateTable
 
 class PolicyIteration(Plans):
-    VALUE_DECIMAL_PRECISION = 10
     def __init__(
         self,
         max_iterations=int(1e5),
@@ -17,56 +17,81 @@ class PolicyIteration(Plans):
         self.max_iterations = max_iterations
         self.undefined_value = undefined_value
 
-    def plan_on(self, mdp: TabularMarkovDecisionProcess):
-        if mdp.dead_end_state_vec.any():
-            warnings.warn("MDP contains states where no actions can be taken. This can have unanticipated effects.")
-        transition_matrix = mdp.transition_matrix.copy()
-        transition_matrix[mdp.recurrent_state_vec,] = 0
-        state_values, action_values, policy_matrix, iterations = policy_iteration_vectorized(
-            transition_matrix=transition_matrix,
-            terminal_state_vector=~mdp.transient_state_vec.astype(bool),
-            discount_rate=mdp.discount_rate,
-            reward_matrix=mdp.reward_matrix,
-            action_matrix=mdp.action_matrix.astype(bool),
-            max_iterations=self.max_iterations
+    def plan_on(self, mdp: TabularMarkovDecisionProcess) -> "PolicyIterationResult":
+        return self.batch_plan_on([mdp])[0]
+    
+    def batch_plan_on(self, mdps: Sequence[TabularMarkovDecisionProcess]) -> Sequence["PolicyIterationResult"]:
+        transition_matrices = np.zeros(
+            (len(mdps), ) + mdps[0].transition_matrix.shape,
+            dtype=mdps[0].transition_matrix.dtype
         )
-        state_values[mdp.recurrent_state_vec,] = self.undefined_value
-        action_values[mdp.recurrent_state_vec,] = self.undefined_value
-            
-        # terminal dead ends are handled by turning them into a uniform distribution
-        # since they are all equally -inf, but non-terminal dead end states 
-        # are allowed to have their single action taken
-        policy_matrix = np.isclose(
-            action_values,
-            np.max(action_values, axis=-1, keepdims=True),
-            atol=10**(-self.VALUE_DECIMAL_PRECISION),
-            rtol=0
+        discount_rates = np.zeros((len(mdps), ), dtype=type(mdps[0].discount_rate))
+        state_action_reward_matrices = np.zeros(
+            (len(mdps), ) + mdps[0].transition_matrix.shape[:-1],
+            dtype=mdps[0].reward_matrix.dtype
         )
-        policy_matrix = policy_matrix/policy_matrix.sum(-1, keepdims=True)
-        single_action_states = mdp.action_matrix.sum(-1) == 1
-        policy_matrix[single_action_states] = mdp.action_matrix[single_action_states]
-        policy=TabularPolicy.from_state_action_lists(
-            state_list=mdp.state_list,
-            action_list=mdp.action_list,
-            data=policy_matrix
+        action_matrices = np.zeros(
+            (len(mdps), ) + mdps[0].transition_matrix.shape[:-1],
+            dtype=bool
         )
-        state_values=StateTable.from_state_list(
-            state_list=mdp.state_list,
-            data=state_values
+        for mdp_i, mdp in enumerate(mdps):
+            if mdp.dead_end_state_vec.any():
+                warnings.warn("MDP contains states where no actions can be taken. This can have unanticipated effects.")
+            if mdp.recurrent_state_vec.any():
+                warnings.warn("MDP contains states that never reach an absorbing state. " +\
+                    f"Values for these states will be set using self.undefined_value={self.undefined_value}"
+                )
+            transition_matrices[mdp_i] = mdp.transition_matrix
+            transition_matrix = transition_matrices[mdp_i]
+            transition_matrix[mdp.recurrent_state_vec,] = 0
+            transition_matrix[mdp.absorbing_state_vec,] = 0
+            discount_rates[mdp_i] = mdp.discount_rate
+            state_action_reward_matrix = np.einsum("san,san->sa", transition_matrix, mdp.reward_matrix)
+            state_action_reward_matrices[mdp_i] = state_action_reward_matrix
+            action_matrices[mdp_i] = mdp.action_matrix.astype(bool)
+        
+        policy_norm = action_matrices.sum(-1, keepdims=True)
+        policy_norm[policy_norm == 0] = 1
+        policy_matrices = action_matrices/policy_norm
+
+        state_values_batch, action_values_batch, policy_matrix_batch, iterations = policy_iteration_vectorized(
+            transition_matrix=transition_matrices,
+            discount_rate=discount_rates,
+            state_action_reward_matrix=state_action_reward_matrices,
+            action_matrix=action_matrices,
+            max_iterations=self.max_iterations,
+            policy_matrix=policy_matrices,
         )
-        action_values=StateActionTable.from_state_action_lists(
-            state_list=mdp.state_list,
-            action_list=mdp.action_list,
-            data=action_values
-        )
-        return PolicyIterationResult(
-            iterations=iterations,
-            state_value=state_values,
-            action_value=action_values,
-            converged=iterations < (self.max_iterations - 1),
-            initial_value=sum([state_values[s]*p for s, p in mdp.initial_state_dist().items()]),
-            policy=policy
-        )
+
+        results = []
+        for mdp, state_values, action_values, policy_matrix in zip(
+            mdps, state_values_batch, action_values_batch, policy_matrix_batch
+        ):
+            state_values[mdp.recurrent_state_vec,] = self.undefined_value
+            action_values[mdp.recurrent_state_vec,] = self.undefined_value
+            policy=TabularPolicy.from_state_action_lists(
+                state_list=mdp.state_list,
+                action_list=mdp.action_list,
+                data=policy_matrix
+            )
+            state_values=StateTable.from_state_list(
+                state_list=mdp.state_list,
+                data=state_values
+            )
+            action_values=StateActionTable.from_state_action_lists(
+                state_list=mdp.state_list,
+                action_list=mdp.action_list,
+                data=action_values
+            )
+            results.append(PolicyIterationResult(
+                iterations=iterations,
+                state_value=state_values,
+                action_value=action_values,
+                converged=iterations < (self.max_iterations - 1),
+                initial_value=sum([state_values[s]*p for s, p in mdp.initial_state_dist().items()]),
+                policy=policy
+            ))
+        return results
 
 @dataclass
 class PolicyIterationResult(PlanningResult):
@@ -79,44 +104,36 @@ class PolicyIterationResult(PlanningResult):
 
 def policy_iteration_vectorized(
     transition_matrix,
-    terminal_state_vector,
     discount_rate,
-    reward_matrix,
+    state_action_reward_matrix,
     action_matrix,
+    policy_matrix,
     max_iterations=int(1e5),
     value_difference=1e-10,
-    policy=None
 ):
     """
     Implementation of regularized policy iteration with
     an inverse temperature parameter of infinity, which
-    yields and equivalent optimal value function.
-
-    This implementation supports arbitrary discounted problems,
-    and undiscounted problems where all non-terminal transitions 
-    have non-positive rewards.
+    yields an equivalent optimal value function.
+    
+    Note that the first dimension of all inputs is a batch dimension.
     """
-    assert terminal_state_vector.dtype == bool
     assert action_matrix.dtype == bool
     action_penalty = np.log(action_matrix)
-    rf_sa = np.einsum("san,san->sa", reward_matrix, transition_matrix)
-    rf_sa[terminal_state_vector] = 0
-    policy_norm = action_matrix.sum(-1, keepdims=True)
-    policy_norm[policy_norm == 0] = 1
-    policy = action_matrix/policy_norm
-
+    n_states = transition_matrix.shape[1]
+    eye = np.eye(n_states)
+    
     for i in range(max_iterations):
-        mp = np.einsum("san,sa->sn", transition_matrix, policy)
-        mp[terminal_state_vector] = 0
-        rf_s = np.einsum("sa,sa->s", rf_sa, policy)
-        v = np.linalg.solve(np.eye(mp.shape[0]) - discount_rate*mp, rf_s)
-        q = rf_sa + (discount_rate*transition_matrix*v[None, None, :]).sum(-1) + action_penalty
-        q[terminal_state_vector] = 0
+        mp = np.einsum("bsan,bsa,b->bsn", transition_matrix, policy_matrix, discount_rate)
+        rf_s = np.einsum("bsa,bsa->bs", state_action_reward_matrix, policy_matrix)
+        v = np.linalg.solve(eye[None,...] - mp, rf_s)
+        next_q = np.einsum("b,bsan,bn->bsa", discount_rate, transition_matrix, v)
+        q = state_action_reward_matrix + action_penalty + next_q 
         new_policy = np.isclose(q, np.max(q, axis=-1, keepdims=True), atol=value_difference, rtol=0)
         new_policy = new_policy/new_policy.sum(-1, keepdims=True)
-        if np.isclose(new_policy, policy).all():
+        if np.isclose(new_policy, policy_matrix, atol=value_difference, rtol=0).all():
             break
-        policy[:] = new_policy
-    policy[~action_matrix] = 0
+        policy_matrix = new_policy
+    policy_matrix[~action_matrix] = 0
     v = np.max(q, axis=-1)
-    return v, q, policy, i
+    return v, q, policy_matrix, i
