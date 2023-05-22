0f53567e3c342f7e28b8cb4022f70b7e6d0c9bf6
markkho <markkho32@gmail.com>
2023-05-22 17:01:46 -0400

    Make A* and BFS automatically coerce deterministic MDPs to DSS

--- msdm/core/mdp/deterministic_shortest_path.py
+++ msdm/core/mdp/deterministic_shortest_path.py
@@ -23,3 +23,25 @@ class DeterministicShortestPathProblem(MarkovDecisionProcess):
     @abstractmethod
     def initial_state(self):
         pass
+
+    @classmethod
+    def from_mdp(cls, mdp: MarkovDecisionProcess) -> 'DeterministicShortestPathProblem':
+        """
+        Safely converts an MDP into a DeterministicShortestPathProblem.
+        """
+        if isinstance(mdp, DeterministicShortestPathProblem):
+            return mdp
+        class DeterministicShortestPathProblemFromMDP(DeterministicShortestPathProblem):
+            def __init__(self): pass
+            def initial_state(self):
+                initial_state = mdp.initial_state_dist().support
+                assert len(initial_state) == 1, "MDP has non-deterministic initial state"
+                return initial_state[0]
+            def next_state(self, s, a):
+                next_state = mdp.next_state_dist(s, a).support
+                assert len(next_state) == 1, "MDP has non-deterministic transition function"
+                return next_state[0]
+        DeterministicShortestPathProblemFromMDP.actions = staticmethod(mdp.actions)
+        DeterministicShortestPathProblemFromMDP.reward = staticmethod(mdp.reward)
+        DeterministicShortestPathProblemFromMDP.is_absorbing = staticmethod(mdp.is_absorbing)
+        return DeterministicShortestPathProblemFromMDP()
